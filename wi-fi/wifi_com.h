fbd0feef756ce373d6521b3cd3d204026c3b8f3c
manucho1586 <101818572+manucho1586@users.noreply.github.com>
2024-08-27 11:48:30 -0300

    upload wi-fi

---
+++ wi-fi/wifi_com.h
@@ -0,0 +1,18 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _WIFI_COM_
+#define _WIFI_COM_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void wifiComRestart();
+void wifiComInit();
+void wifiComUpdate();
+
+//=====[#include guards - end]=================================================
+
+#endif /* _WIFI_COM_ */
